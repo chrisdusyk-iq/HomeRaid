02700a193f85f445b08d2c246bbd0699fc22afb2
Kevin Williams <Kevin.Williams@iQmetrix.com>
2019-01-26 12:31:51 -0600

    Ant controls. Move slowly, so that abrupt stops don't feel jarring to the player.

---
+++ HomeRaid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody _rb;
+	public float ForwardMovement = 25f;
+	public float RotationSpeed = 4f;
+
+	// Start is called before the first frame update
+	public void Start()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	public void Update()
+	{
+		ProcessForwardMotionInput();
+		ProcessRotationInput();
+	}
+
+	private void ProcessForwardMotionInput()
+	{
+		var isMovingAlongForwardAxis = false;
+
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			isMovingAlongForwardAxis = true;
+			MoveAlongForwardAxis(ForwardMovement * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			isMovingAlongForwardAxis = true;
+			MoveAlongForwardAxis(-ForwardMovement * Time.deltaTime);
+		}
+
+		if (!isMovingAlongForwardAxis)
+			StopMoving();
+	}
+
+	private void MoveAlongForwardAxis(float forwardForceToAdd)
+		=> _rb.AddRelativeForce(0, forwardForceToAdd, 0);
+
+	private void StopMoving()
+		=> _rb.velocity = Vector3.zero;
+
+	private void ProcessRotationInput()
+	{
+		var isRotating = false;
+
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			isRotating = true;
+			RotateForwardAxis(RotationSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			isRotating = true;
+			RotateForwardAxis(-RotationSpeed * Time.deltaTime);
+		}
+
+		if (!isRotating)
+			StopRotating();
+	}
+
+	private void RotateForwardAxis(float torqueToAdd)
+	{
+		_rb.freezeRotation = false;
+		_rb.AddRelativeTorque(0, 0, torqueToAdd);
+	}
+
+	private void StopRotating()
+		=> _rb.freezeRotation = true;
+}

a7f2b5db872684ca1909d4010dbb1f025287b3c2
Chris Dusyk <chrisd@iqmetrix.com>
2019-01-26 13:07:05 -0600

    Add persistent scene

--- HomeRaid/Assets/Code/MonoBehaviours/SceneControl/SceneController.cs
+++ HomeRaid/Assets/Code/MonoBehaviours/SceneControl/SceneController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private IEnumerable<string> _sceneList = Enumerable.Empty<string>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	private void Start()
+	{
+		// Create static list of scenes
+		_sceneList = new List<string>
+		{
+			"Scene1"
+		};
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
 }

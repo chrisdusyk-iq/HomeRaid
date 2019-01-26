a00d138029f1f861a11b7793bd5d9fd6d33e7a4c
Chris Dusyk <chrisd@iqmetrix.com>
2019-01-26 15:19:30 -0600

    Create reaction and condition classes

---
+++ HomeRaid/Assets/Code/ScriptableObject/Interactions/Conditions/ConditionCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionCollection : ScriptableObject
+{
+	public string Description;
+	public Condition[] RequiredConditions = new Condition[0];
+	public ReactionCollection ReactionCollection;
+
+
+	public bool CheckAndReact()
+	{
+		for (int i = 0; i < RequiredConditions.Length; i++)
+		{
+			if (!AllConditions.CheckCondition(RequiredConditions[i]))
+				return false;
+		}
+
+		if (ReactionCollection)
+			ReactionCollection.React();
+
+		return true;
+	}
+}
---
+++ HomeRaid/Assets/Code/ScriptableObject/Interactions/Reactions/SceneReaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SceneReaction : Reaction
+{
+	public string sceneName;
+
+	private SceneController _sceneController;
+
+	protected override void SpecificInit() => _sceneController = FindObjectOfType<SceneController>();
+
+	protected override void ImmediateReaction()
+	{
+		_sceneController.ProgressScene();
+	}
+}

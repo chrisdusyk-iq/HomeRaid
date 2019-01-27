8fab53e9381902eb5a37db0865ca8d77c3f97a0a
Shaun McHugh <ShaunM@iqmetrix.com>
2019-01-27 03:04:42 -0600

    adding jumping spider

---
+++ HomeRaid/Assets/SpiderMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float jumpSpeed = 1.0f;
+    public float gravity = 10.0f;
+    Vector3 direction = Vector3.zero;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (controller.isGrounded)
+        {
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            direction *= speed;
+
+            direction.y = jumpSpeed;
+
+        }
+        direction.y -= gravity * Time.deltaTime;
+        controller.Move(direction * Time.deltaTime);
+    }
+}

21773516e045a0d70a62b46ca91c9aec4cfc1ec2
Chris Dusyk <chrisd@iqmetrix.com>
2019-01-27 11:42:44 -0600

    Progress level on collision with house

---
+++ HomeRaid/Assets/Code/MonoBehaviours/Interactions/HouseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseCollider : MonoBehaviour
+{
+	private SceneController sceneController;
+
+	void Start()
+	{
+		sceneController = FindObjectOfType<SceneController>();
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		sceneController.ProgressScene();
+	}
+}

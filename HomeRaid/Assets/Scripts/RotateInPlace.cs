994c3ce27c2e421a2d03c352b587c4c708612a77
Kevin Williams <Kevin.Williams@iQmetrix.com>
2019-01-27 13:25:48 -0600

    Update the Game Over screen to a happier "You Win!" scene.

---
+++ HomeRaid/Assets/Scripts/RotateInPlace.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotateInPlace : MonoBehaviour
+{
+	public float RotationSpeed = 10f;
+
+	// Update is called once per frame
+	public void Update()
+		=> transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+}
